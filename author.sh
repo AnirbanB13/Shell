46f534ab650a278c4070fdbaf282bf96c27235c7
AnirbanB13 <anirbanbanerjeeuv@gmail.com>
2026-05-17 18:42:08 +0530

    added test file

---
+++ author.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# just a placeholder file to run all the arithmetic operations in one go. This file is not meant to be executed directly, but rather to be sourced by test.sh which will call each of the individual operation scripts.
